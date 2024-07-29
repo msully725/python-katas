224c838e461a0c0500d9b9222eee45f094a4deb9
sully <sully@sullivandigitalconsulting.com>
2024-07-29 08:48:59 -0400

    Parmaterize vector multiplication test

--- numba_katas/2_vectorizing_operations/test_vectorized_multiplication.py
+++ numba_katas/2_vectorizing_operations/test_vectorized_multiplication.py
@@ -1,9 +1,13 @@
 import numpy as np
+import pytest
 from vectorized_multiplication import vectorized_multiplication
 
-def test_vectorized_multiplication():
-    a = np.array([1, 2, 3, 4])
-    b = np.array([5, 6, 7, 8])
-    result = vectorized_multiplication(a, b)
-    expected = np.array([5, 12, 21, 32])
-    np.testing.assert_array_equal(result, expected)
+@pytest.mark.parametrize("a, b, expected", [
+    ([1, 2, 3, 4], [5, 6, 7, 8], [5, 12, 21, 32]),
+])
+def test_vectorized_multiplication(a, b, expected):
+    numpy_a = np.array(a)
+    numpy_b = np.array(b)
+    result = vectorized_multiplication(numpy_a, numpy_b)
+    numpy_expected = np.array(expected)
+    np.testing.assert_array_equal(result, numpy_expected)
